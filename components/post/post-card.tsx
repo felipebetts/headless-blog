a0c034e4bd64e8e102cb896af33a218e9e61b132
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2023-01-27 18:14:29 -0300

    update post card

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -10,6 +10,7 @@ interface Props {
     tags?: Array<string>
     title: string
     thumbnailUrl: string
+    minutesToRead: number
 }
 
 const PostCard: React.FC<Props> = ({
@@ -17,7 +18,8 @@ const PostCard: React.FC<Props> = ({
     slug,
     tags,
     title,
-    thumbnailUrl
+    thumbnailUrl,
+    minutesToRead,
 }) => {
   return (
     <div className="w-full h-full">
@@ -27,21 +29,34 @@ const PostCard: React.FC<Props> = ({
                 className={`bg-cover bg-center h-72`}
             />
         </NextLink>
-        {tags && (
-            <div className="flex items-center w-full">
-                {tags.map((tag, i) => i === tags.length - 1 ? (
-                    <Link href={`/categorias/${tag}`} key={`${slug}_${tag}`}>
+        <div className="flex items-center text-xs text-gray-400">
+            {tags && (
+                <>
+                    {tags.map((tag, i) => i === tags.length - 1 ? (
+                        <Link href={`/categorias/${tag}`} key={`${slug}_${tag}`}>
                             { tag }
-                        </Link>
-                ) : (
-                    <>
-                        <Link href={`/categorias/${tag}`} key={`${slug}_${tag}`} px={0}>
-                            { tag }
-                        </Link>
-                    </>
-                ))}
-            </div>
-        )}
+                            </Link>
+                    ) : (
+                        <>
+                            <Link href={`/categorias/${tag}`} key={`${slug}_${tag}`} px={0}>
+                                { tag }
+                            </Link>
+                        </>
+                    ))}
+                    <span className='pr-2'> • </span>
+                </>
+            )}
+            {minutesToRead && (
+                <div className="flex items-center text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span className='ml-1'>
+                    {minutesToRead} minuto{minutesToRead > 1 ? 's' : ''} de leitura
+                </span>
+                </div>
+            )}
+        </div>
         <NextLink href={`/posts/${slug}`}>
             <h4 className='text-lg hover:text-indigo-700 transition-all duration-150 font-medium'>
                 {title}
