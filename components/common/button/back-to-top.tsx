9edbbc87e6d886b6666bdeab64c29ff650a3fe60
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2023-01-30 20:29:15 -0300

    smooth scroll

--- components/common/button/back-to-top.tsx
+++ components/common/button/back-to-top.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react'
 import NextLink from 'next/link'
 import { ScrollContext } from '@/utils/scroll-observer'
 import useWindowDimensions from '@/hooks/use-window-dimensions'
+import classNames from '@/utils/classnames'
 
 const BackToTopButton = () => {
 
@@ -17,20 +18,26 @@ const BackToTopButton = () => {
     }
   }, [height, scrollY])
 
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }
+
   useEffect(() => {
     handleShowButton()
   }, [scrollY, height, handleShowButton])
 
-  if (!show) {
-    return <></>
-  }
-
   return (
-      <NextLink href="#top" className='fixed bottom-6 right-6 rounded-md bg-slate-300 hover:bg-slate-200 w-8 h-8 flex justify-center items-center drop-shadow-md hover:drop-shadow-none transition-all duration-100'>
+      <button
+        className={classNames(
+          'fixed bottom-6 right-6 rounded-md bg-slate-300 hover:bg-slate-200 w-8 h-8 flex justify-center items-center drop-shadow-md hover:drop-shadow-none transition-all duration-100',
+          show ? 'block' : 'hidden'
+        )}
+        onClick={handleScrollTop}
+      >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6">
           <path fillRule="evenodd" d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z" clipRule="evenodd" />
         </svg>
-      </NextLink>
+      </button>
   )
 }
 
